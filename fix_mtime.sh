e5f2e164f6dd30c848919f305fe7c06e38175e6c
pavel-voinov <voinovpa@gmail.com>
2015-11-29 19:44:16 +0000

    Script to fix modified time of the media file according to its metadata

---
+++ fix_mtime.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+f="$1"
+
+FNAME=`basename "$f"`
+FDIR=`dirname "$f"`
+F=`echo "$FNAME" | tr '[:lower:]' '[:upper:]'`
+e="${F##*.}"
+if [ "$e" = 'LOG' ]; then
+  exit 0
+fi
+
+get_video_timestamp () {
+  local TS=`mediainfo --Inform="Video;%Encoded_Date%" "$1" 2>/dev/null | cut -d ' ' -f 2,3 --output-delimiter='_' | sed -r 's/:/-/g'`
+  echo "$TS"
+}
+
+get_image_timestamp () {
+  local TS=`exiv2 -g 'Exif.Photo.DateTimeOriginal' print "$1" 2>/dev/null | sed -r 's/\s+/\t/g' | cut -f4- | sed -r 's/\t/_/g;s/:/-/g'`
+  echo "$TS"
+}
+
+get_file_timestamp () {
+  local TS=`date -r "$1" +%Y-%m-%d_%H-%M-%S`
+  echo "$TS"
+}
+
+if [ -f "$f" ]; then
+  case "$e" in
+    JPG|CR2|TIFF|JPEG)
+      D=`get_image_timestamp "$f"`
+      T='IMAGE'
+      ;;
+    MP4|MOV|AVI|MPG|MPEG)
+      D=`get_video_timestamp "$f"`
+      T='VIDEO'
+      ;;
+    *)
+      D=''
+      M=''
+      T='UNKNOWN'
+      ;;
+  esac
+
+  if [ -z "$D" ]; then
+    D=`get_file_timestamp "$f"`
+  fi
+
+  if [ "$T" = 'IMAGE' ]; then
+    exiv2 -T rename "$f"
+  else
+    echo "$f - $D"
+  fi
+fi
