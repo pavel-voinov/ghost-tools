4589d7652c113088b02f59af63830de1581a908e
pavel-voinov <voinovpa@gmail.com>
2015-12-06 21:43:59 +0000

    Improved method to modify timestamp - by file name or metadata for non-image files too

--- fix_mtime.sh
+++ fix_mtime.sh
@@ -13,11 +13,20 @@ get_video_timestamp () {
 
 get_image_timestamp () {
   local TS=`exiv2 -g 'Exif.Photo.DateTimeOriginal' print "$1" 2>/dev/null | sed -r 's/\s+/\t/g' | cut -f4- | sed -r 's/\t/_/g;s/:/-/g'`
-  echo "$TS"
+  if [ $? -eq 0 ]; then
+    echo "$TS"
+  fi
 }
 
 get_file_timestamp () {
   local TS=`date -r "$1" +%Y-%m-%d_%H-%M-%S`
+  if [ $? -eq 0 ]; then
+    echo "$TS"
+  fi
+}
+
+get_timestamp_by_filename () {
+  local TS=`basename "$1" | cut -c 1-19 | egrep '[0-9]{4}-(0[1-9]|1[012])-[0-9]{2}_[0-9]{2}-[0-9]{2}-[0-9]{2}'`
   echo "$TS"
 }
 
@@ -38,12 +47,27 @@ if [ -f "$f" ]; then
   esac
 
   if [ -z "$D" ]; then
-    D=`get_file_timestamp "$f"`
+    D=`get_timestamp_by_filename "$f"`
+    flag=1
+  else
+    flag=0
   fi
 
-  if [ "$T" = 'IMAGE' ]; then
-    exiv2 -T rename "$f"
-  else
-    echo "`date -r $f` vs $D"
+  if [ -n "$D" ]; then
+    FSTAMP=`get_file_timestamp "$f"`
+    if [ "$FSTAMP" != "$D" ]; then
+      if [[ $flag -eq 0 && "$T" = 'IMAGE' ]]; then
+        echo 'Change of modification timstamp by Exif metadata'
+        exiv2 -T rename "$f"
+      else
+        if [ $flag -eq 1 ]; then
+          echo 'Change of modification timstamp by file name'
+        else
+          echo 'Change of modification timstamp by metadata'
+        fi
+        STAMP=`echo $D | sed -r 's/_//g;s/-/./4;s/-//g'`
+        touch --no-create -t $STAMP "$f"
+      fi
+    fi
   fi
 fi
