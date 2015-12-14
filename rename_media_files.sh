aecae0f352e77a65df16e185a81d5bb5b293170f
pavel-voinov <voinovpa@gmail.com>
2015-12-14 22:16:09 +0000

    minor change

--- rename_media_files.sh
+++ rename_media_files.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 pwd
-logfile="rename_`date +%Y-%m-%d_%H-%M-%S`.log"
+logfile="/tmp/rename_`pwd | sed -r 's@/@_@g'`_`date +%Y-%m-%d_%H-%M-%S`.log"
+pwd >> $logfile
 find * -maxdepth 0 -type f -exec rename_media_file.sh "{}" \; 2>&1 | tee -a $logfile
 
 echo "logfile is $logfile"
