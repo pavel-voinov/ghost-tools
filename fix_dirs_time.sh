26ca7d5a9c32b2cb95bc15eecae417c0049dcc69
pavel-voinov <voinovpa@gmail.com>
2016-04-19 21:02:31 +0100

    New script to set proper date for dirs YYYY/MM

---
+++ fix_dirs_time.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+pushd /media/NS4300N/Photos
+
+for y in ????; do
+  touch --no-create -t "${y}01010000" $y
+done
+
+for d in ????/??; do
+  y=`echo $d | sed -r 's@/@@'`
+  touch --no-create -t "${y}010000" $d
+done
+
+popd
