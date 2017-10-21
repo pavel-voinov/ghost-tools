cb388f1a1cd662abbf6263ca3331f7d0605a51d8
pavel-voinov <voinovpa@gmail.com>
2017-10-21 19:39:20 +0100

    Add renaming of assiciated files with the same file name but different extension

--- rename_media_file.sh
+++ rename_media_file.sh
@@ -6,9 +6,32 @@ FNAME=`basename "$f"`
 FDIR=`dirname "$f"`
 F=`echo "$FNAME" | tr '[:lower:]' '[:upper:]'`
 e="${F##*.}"
-if [[ "$e" = 'LOG' || "$e" = 'INI' ]]; then
+
+image_extensions=('JPG' 'CR2' 'TIFF' 'JPEG' 'PNG')
+video_extensions=('MP4' 'MOV' 'AVI' 'MPG')
+media_extensions=("${image_extensions[@]}" "${video_extensions[@]}")
+
+# source: https://stackoverflow.com/questions/14366390/check-if-an-element-is-present-in-a-bash-array
+array_contains2 () { 
+  local array="$1[@]"
+  local seeking=$2
+  local in=1
+  for element in "${!array}"; do
+    if [[ $element == $seeking ]]; then
+      in=0
+      break
+    fi
+  done
+  return $in
+}
+
+array_contains2 image_extensions "$e" && IS_IMAGE=1
+array_contains2 video_extensions "$e" && IS_VIDEO=1
+
+if [[ $IS_VIDEO -ne 1 && $IS_IMAGE -ne 1 ]]; then
   exit 0
 fi
+
 logfile="rename_`date +%Y-%m-%d_%H-%M-%S`.log"
 
 get_video_timestamp () {
@@ -50,43 +73,48 @@ index_name () {
   echo "$__n"
 }
 
-if [ -f "$f" ]; then
-  if [[ "$e" == 'JPG' || "$e" == 'CR2' || "$e" == 'TIFF' || "$e" == 'JPEG' ]]; then
+if [[ -f $f ]]; then
+  if [[ $IS_IMAGE -eq 1 ]]; then
     D=`get_image_timestamp "$f"`
     M=`get_image_camera_model "$f"`
-    T='IMAGE'
-  elif [[ "$e" == 'MP4' || "$e" == 'MOV' || "$e" == 'AVI' || "$e" == 'MPG' ]]; then
+  elif [[ $IS_VIDEO -eq 1 ]]; then
     D=`get_video_timestamp "$f"`
     M=`get_video_camera_model "$f"`
-    T='VIDEO'
   else
     D=''
     M=''
-    T='UNKNOWN'
   fi
 
-  if [ -z "$D" ]; then
+  if [[ -z $D ]]; then
     echo "File \"$f\". No Exif data found. TS: `get_file_timestamp "$f"`"
-    if [ "$use_ts" == 'Y' ]; then
+    if [[ $use_ts == 'Y' ]]; then
       D=`get_file_timestamp "$f"`
-      M=`echo "${FNAME%.*}" | sed -r 's/[^[:alnum:]_#]/_/g'`
+      M=`echo "${FNAME%.*}" | sed -r 's/[^[:alnum:]_#]+/_/g'`
     fi
   fi
 
-  if [ -n "$D" ]; then
-    if [ -n "$M" ]; then
+  if [[ -n $D ]]; then
+    if [[ -n $M ]]; then
       n=$( index_name "${D}_${M}" "$e" )
     else
       n=$( index_name "${D}" "$e" )
     fi
 
     if [[ "$FNAME" != "$n" && "$n" != ".$e" ]]; then
-      if [ "$T" = 'IMAGE' ]; then
+      if [[ $IS_IMAGE -eq 1 ]]; then
         exiv2 -T rename "$f"
       fi
 #      cp -v --no-preserve=ownership "$f" "$FDIR/$n" && rm -v "$f"
       mv -v "$f" "$FDIR/$n"
+      nfname=${n%%.*}
+      # Rename associated service files too
+      for o in `ls "${f%%.*}".* 2>/dev/null`; do
+        oe=`echo ${o##*.} | tr 'a-z' 'A-Z'`
+        array_contains2 media_extensions "$oe" && tst=1
+        if [[ $tst -ne 1 ]]; then
+          mv -v "$o" "$FDIR/$nfname.$oe"
+        fi
+      done
     fi
   fi
 fi
-
